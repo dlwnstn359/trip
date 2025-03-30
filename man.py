ef629387c7d2a9c89e434acef929f68a4d62fa2f
dlwnstn359 <dlwnstn519@gmail.com>
2025-03-30 21:10:14 +0900

    Update man.py

--- man.py
+++ man.py
@@ -29,6 +29,9 @@ else:
             if page == "메인":
                 st.title("메인 페이지")
                 st.write("메인 페이지 내용입니다.")
+                if st.button("ㅇ"):
+                    st.subheader("ㅇㅇ")
+                    st.write("dldkdaj")
                 # 예시: st.image("main_image.jpg")
             elif page == "1코스":
                 st.title("1코스 페이지")
